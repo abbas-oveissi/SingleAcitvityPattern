6fc856edf393f99cadc037a7fe8144c3fdb1c572
abbas <abbas.oveissi@gmail.com>
2018-08-12 19:07:32 +0430

    improve NavigationManager finder mechanism

--- app/src/main/java/ir/oviessi/singleactivitypattern/bases/BaseFragment.kt
+++ app/src/main/java/ir/oviessi/singleactivitypattern/bases/BaseFragment.kt
@@ -4,37 +4,54 @@ import android.app.Activity
 import android.content.Context
 import android.support.v4.app.Fragment
 import ir.oviessi.singleactivitypattern.NavigationManager
+import android.R.attr.fragment
+
 
 /**
  * Created by abbas on 1/22/18.
  */
-open class BaseFragment: Fragment() {
+open class BaseFragment : Fragment() {
 
-    private lateinit var navigationManagerInner:NavigationManager
-    lateinit var fragmentInteractionInner:FragmentInteractionListener
+    private lateinit var navigationManagerInner: NavigationManager
+    lateinit var fragmentInteractionInner: FragmentInteractionListener
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
 
-        if(parentFragment!=null && parentFragment is HasNavigationManager)
-                navigationManagerInner=(parentFragment as HasNavigationManager).provideNavigationManager()
-        else if (context is HasNavigationManager)
-                navigationManagerInner=(context as HasNavigationManager).provideNavigationManager()
-        else
-            throw RuntimeException("Activity host must implement HasNavigationManager")
 
 
-        if(context is Activity)
-                fragmentInteractionInner=context as FragmentInteractionListener
+        navigationManagerInner = findNavigationManager()
+
+
+        if (context is Activity)
+            fragmentInteractionInner = context as FragmentInteractionListener
         else
             throw RuntimeException("Activity host must implement FragmentInteractionListener")
 
     }
 
+    fun findNavigationManager(): NavigationManager {
+        var parentFrag: Fragment? = BaseFragment@ this
+        while (true) {
+            parentFrag = parentFrag?.parentFragment
 
-    fun getNavigationManager():NavigationManager=navigationManagerInner
+            if (parentFrag == null)
+                break
 
-    open fun onBackPressed(): Boolean= false
+            if (parentFrag is HasNavigationManager) {
+                return parentFrag as NavigationManager
+            }
+        }
+
+        if (context is HasNavigationManager)
+            return (context as HasNavigationManager).provideNavigationManager()
+
+        throw IllegalArgumentException("No NavigationManager was found")
+    }
+
+    fun getNavigationManager(): NavigationManager = navigationManagerInner
+
+    open fun onBackPressed(): Boolean = false
 
     override fun onStart() {
         super.onStart()
